0e7db56f2b8b808c62cf35523239088b15d32795
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-02-24 14:43:53 -0600

    Added sftp support
    Added the posibility to use a local or linux server location. Limited to user-password authentication. The locations and sftp credentials can only be modified directly in the config.json file

--- gui.py
+++ gui.py
@@ -3,33 +3,65 @@ from tkinter import END, Button, Scrollbar, ttk
 import db_query as query
 import main_script as script
 import os, time
+from pathlib import Path, PurePosixPath
+import pysftp
 
+
+# BUTTON FUNCTIONS
+#! MEJORAR MANEJO DE ERRORES. ELIMINAR IF ANIDADOS
 def scan_button ():
     shows = []
     shows = query.all_shows("show")
-    path = cb_save.get()
-    update_log(f"Carpeta de Descarga: {path}")
+    alias_path = cb_save.get()
+    path_info = query.save_paths_dict(alias_path)
+    succes1 = False
+    succes2 = False
+    update_log(f"Descarga en {alias_path}")
     
     for show in shows:
         show_data = query.show_data(show)
         next_episode = show_data["current_episode"]+1
+        
         ep_url = show_data["list_url"].replace("/anime/","/ver/")+ "-" + str(next_episode)
         
         if script.new_episode(ep_url):
             update_log(f"{show} episodio {next_episode} disponible")
             update_log("Obteniendo enlace de descarga...")
             url_zippy = script.scrap_download_link(ep_url)
-            episode_name= f"{show}_{str(next_episode)}.mp4"
+            
+            # ADD 0 TO NAME
+            if next_episode < 10:
+                episode_name= f"E0{str(next_episode)}.mp4"
+            else:
+                episode_name= f"E{str(next_episode)}.mp4"
 
             if url_zippy != False:
-                mp4_link = script.z_scrap(url_zippy)
+                mp4_link = script.zipp_scrap(url_zippy)
 
                 if mp4_link != False:
                     update_log(f"Descargando {episode_name}")
+                    temp_path = Path("D:/Temp/CAPITULOS DESCARGA/Temporal")
                     
-                    if script.download(mp4_link,episode_name, path):
+                    # SAVE EPISODE TO LOCAL PATH, THEN MOVE IT
+                    succes1 = script.download(mp4_link, episode_name, temp_path)
+                    
+                    if path_info["transmission"] == "local":
+                        dest_path = Path(path_info["directory"], show_data["folder_name"]) 
+                    elif path_info["transmission"] == "sftp":
+                        dest_path = PurePosixPath(path_info["directory"], show_data["folder_name"])
+                    else:
+                        update_log("Save´s path transmission type invalid")
+                        
+                    succes2 = script.allocate_to_directory(path_info, 
+                                                           temp_path, 
+                                                           dest_path, 
+                                                           episode_name)
+                    
+                    if succes1 and succes2:
                         update_log("Descarga finalizada")
-                        query.update_chapter(show)
+                        
+                        query.increase_chapter(show)
+                        
                     else:
                         update_log("Descarga fallida")
                 else:
@@ -44,6 +76,7 @@ def scan_button ():
         os.system("shutdown -s -t " + 10)
         time.sleep(5)        
         quit()
+
         
 def add_button():
     show = bx_show.get()
@@ -68,88 +101,108 @@ def add_button():
     else:
         update_log("Show ya existente, revisar nombre o url")
 
+
 def delete_button ():
     show = cb_show.get()
     query.delete_show(show)
     update_log(f"{show} eliminado")
     cb_show["values"]= query.all_shows("show")
 
+# UTILITY FUNCTIONS
 def update_log (message):
     tx_logs.insert(END,f"{message}\n")
 
 
-# VENTANA GENERAL
-window = tkt.Tk()
-window.title("Descargador de AnimeFlv")
-scrollbar = Scrollbar(window, orient= "vertical")
-lb_messages = tkt.Label(window, text= "Mensajes:")
-tx_logs = tkt.Text(window, yscrollcommand= scrollbar.set, height= 4, width=40)
+def path_filtering (alias, *columns):
+    path_list = query.save_paths_dict()
+    
+    for line in path_list:
+        
+        if line["alias"] == alias:
+            result = []
+            
+            for element in columns:
+                result.append(line[element])
+    
+    return result
 
-#Añadir pestañas
-tab_control = ttk.Notebook (window)
+# SELF-RUNING VALIDATION
+if __name__ == "__main__":    
+    # VENTANA GENERAL
+    window = tkt.Tk()
+    window.title("Descargador de AnimeFlv")
+    scrollbar = Scrollbar(window, orient= "vertical")
+    lb_messages = tkt.Label(window, text= "Mensajes:")
+    tx_logs = tkt.Text(window, yscrollcommand= scrollbar.set, height= 4, width=40)
 
-tab_scan = ttk.Frame(tab_control)
-tab_control.add(tab_scan, text="Escanear y Descargar")
+    #Añadir pestañas
+    tab_control = ttk.Notebook (window)
 
-tab_add = ttk.Frame(tab_control)
-tab_control.add(tab_add, text="Agregar Show")
+    tab_scan = ttk.Frame(tab_control)
+    tab_control.add(tab_scan, text="Escanear y Descargar")
 
-tab_delete = ttk.Frame(tab_control)
-tab_control.add(tab_delete, text="Eliminar Show")
+    tab_add = ttk.Frame(tab_control)
+    tab_control.add(tab_add, text="Agregar Show")
 
-tab_control.grid(row=0,column=0)
-# Pestaña ESCANEAR
-lb_scan = tkt.Label(tab_scan, text= "Escanear y descargar nuevos capitulos")
-bt_scan = tkt.Button(tab_scan, text= "Escanear y Descargar", command= scan_button)
-lb_save = tkt.Label(tab_scan, text= "Save Location:")
-cb_save = ttk.Combobox(tab_scan,state="readonly", values=["//192.168.100.1/usb-058f-CE8B2A_1/Video/Descargas","D:/Temp/CAPITULOS DESCARGA"])
-lb_shutdown = tkt.Label(tab_scan, text= "Apagar tras finalizar:")
-cb_shutdown = ttk.Combobox(tab_scan,state="readonly", values=["Si","No"])
+    tab_delete = ttk.Frame(tab_control)
+    tab_control.add(tab_delete, text="Eliminar Show")
 
-#Pestaña AGREGAR
-lb_add_desc = tkt.Label(tab_add, text="Ingrese los datos para agregar show a seguimiento")
-lb_show = tkt.Label(tab_add, text="Nombre:")
-lb_chapter = tkt.Label(tab_add, text="Captiulo Actual:")
-lb_url = tkt.Label(tab_add, text="URL de lista:")
+    tab_control.grid(row=0,column=0)
 
-bx_show = tkt.Entry(tab_add, width= 10)
-bx_chapter = tkt.Entry(tab_add, width= 10)
-bx_url = tkt.Entry(tab_add, width= 15)
+    # Pestaña ESCANEAR
+    lb_scan = tkt.Label(tab_scan, text= "Escanear y descargar nuevos capitulos")
+    bt_scan = tkt.Button(tab_scan, text= "Escanear y Descargar", command= scan_button)
+    lb_save = tkt.Label(tab_scan, text= "Save Location:")
+    cb_save = ttk.Combobox(tab_scan,state="readonly")
+    list_path = list(element["alias"] for element in query.save_paths_dict())
+    cb_save["values"] = list_path
+    lb_shutdown = tkt.Label(tab_scan, text= "Apagar tras finalizar:")
+    cb_shutdown = ttk.Combobox(tab_scan,state="readonly", values=["Si","No"])
 
-bt_add = tkt.Button(tab_add,text="Agregar", command= add_button)
+    #Pestaña AGREGAR
+    lb_add_desc = tkt.Label(tab_add, text="Ingrese los datos para agregar show a seguimiento")
+    lb_show = tkt.Label(tab_add, text="Nombre:")
+    lb_chapter = tkt.Label(tab_add, text="Captiulo Actual:")
+    lb_url = tkt.Label(tab_add, text="URL de lista:")
 
-#Pestaña ELIMINAR
-lb_del_desc = tkt.Label(tab_delete, text= "Seleccione el elemento a eliminar")
-lb_del_show = tkt.Label(tab_delete, text="Show:")
-cb_show = ttk.Combobox(tab_delete,state="readonly", values=query.all_shows("show"))
-bt_delete = tkt.Button(tab_delete, text= "Eliminar", command= delete_button)
+    bx_show = tkt.Entry(tab_add, width= 10)
+    bx_chapter = tkt.Entry(tab_add, width= 10)
+    bx_url = tkt.Entry(tab_add, width= 15)
 
-# ---- ACOMODO ELEMENTOS ----
-lb_messages.grid(row=1,column=0)
-tx_logs.grid(row=2,column=0)
+    bt_add = tkt.Button(tab_add,text="Agregar", command= add_button)
 
-#Pestaña Escanear
-lb_scan.grid(row=0,columnspan=2)
-lb_save.grid(row=1, column=0)
-cb_save.grid(row=1, column=1)
-lb_shutdown.grid(row=2, column=0)
-cb_shutdown.grid(row=2, column=1)
-bt_scan.grid(row=3,columnspan=2)
+    #Pestaña ELIMINAR
+    lb_del_desc = tkt.Label(tab_delete, text= "Seleccione el elemento a eliminar")
+    lb_del_show = tkt.Label(tab_delete, text="Show:")
+    cb_show = ttk.Combobox(tab_delete,state="readonly", values=query.all_shows("show"))
+    bt_delete = tkt.Button(tab_delete, text= "Eliminar", command= delete_button)
 
-#Pestaña Agregar
-lb_add_desc.grid(row=0,columnspan=2)
-lb_show.grid(row=1,column=0)
-bx_show.grid(row=1,column=1)
-lb_chapter.grid(row=2,column=0)
-bx_chapter.grid(row=2,column=1)
-lb_url.grid(row=3,column=0)
-bx_url.grid(row=3,column=1)
-bt_add.grid(row=4,columnspan=2)
+    # ---- ACOMODO ELEMENTOS ----
+    lb_messages.grid(row=1,column=0)
+    tx_logs.grid(row=2,column=0)
 
-# Pestaña Eliminar
-lb_del_desc.grid(row=0,columnspan=2)
-lb_del_show.grid(row=1,column=0)
-cb_show.grid(row=1,column=1)
-bt_delete.grid(row=2,column=1)
+    #Pestaña Escanear
+    lb_scan.grid(row=0,columnspan=2)
+    lb_save.grid(row=1, column=0)
+    cb_save.grid(row=1, column=1)
+    lb_shutdown.grid(row=2, column=0)
+    cb_shutdown.grid(row=2, column=1)
+    bt_scan.grid(row=3,columnspan=2)
 
-window.mainloop()
+    #Pestaña Agregar
+    lb_add_desc.grid(row=0,columnspan=2)
+    lb_show.grid(row=1,column=0)
+    bx_show.grid(row=1,column=1)
+    lb_chapter.grid(row=2,column=0)
+    bx_chapter.grid(row=2,column=1)
+    lb_url.grid(row=3,column=0)
+    bx_url.grid(row=3,column=1)
+    bt_add.grid(row=4,columnspan=2)
+
+    # Pestaña Eliminar
+    lb_del_desc.grid(row=0,columnspan=2)
+    lb_del_show.grid(row=1,column=0)
+    cb_show.grid(row=1,column=1)
+    bt_delete.grid(row=2,column=1)
+
+    window.mainloop()
--- main_script.py
+++ main_script.py
@@ -1,7 +1,10 @@
 from requests_html import HTMLSession
-import db_query as query
+from pathlib import Path, PurePosixPath
+import os
+import pysftp
+from typing import Union
 
-
+#! TO MIMPROVE SESSION HANDLING WITH 'with'
 def new_episode(url):
     session = HTMLSession()
     r = session.get(url,timeout=10000)
@@ -23,7 +26,7 @@ def scrap_download_link (anime_episode_url):
     return(False)
 
 # ----- SCRAP EN PARA BUSCAR LINK MP4
-def z_scrap (url_zip):
+def zipp_scrap (url_zip):
     session = HTMLSession()
     r_scrap = session.get(url_zip, timeout=10000)
     
@@ -52,10 +55,74 @@ def download (url_mp4, ep_name, save_path):
             for chunk in r.iter_content(chunk_size=chunk_size):
                 f.write(chunk)
         
-        return(True)
+        return True
     except:
-        return(False)
+        return False
 
-def send_sftp ():
-    pass
+def allocate_to_directory (path_config: dict, source: Path, destination: Union[Path, PurePosixPath], filename: str):
+    """
+        Parameters:
+        ----------
+        
+        path_config: dict
+            Information of the destiny path
+        source: Path
+            Directory of the folder, where the file is.
+        destination: Path or PurePosixPath
+            Directory of the desired destination folder. Can be Path or PurePosixPath,
+            depending of m_type.
+        filename: str
+            Name of the file, including extension.
+        
 
+        Returns:
+        --------
+            bool
+                True if successfully moved.
+                False if an exception occurred.
+    """
+    # Local limitation: Only moves within the same disk
+    # Not using shutil.move by personal preferance, since I don´t want
+    # to copy, paste and delete, only move.
+    
+    # SELECT THE TYPE OF ALLOCATION
+    if path_config["transmission"] == "local":
+        try:
+            if not source.exists():
+                return False
+            
+            if not destination.exists():
+                destination.mkdir()
+                
+            os.rename(src= source / filename, dst= destination / filename)
+            return True
+        
+        except:
+            return False
+        
+    elif path_config["transmission"] == "sftp":
+        try:
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys = None
+
+            with pysftp.Connection(path_config["host"], 
+                                username = path_config["user"], 
+                                password = path_config["pass"], 
+                                cnopts = cnopts) as sftp:
+                
+                #CHECK DESTINATION FOLDER, IF DOESNT EXISTS, CREATES IT    
+                if not sftp.isdir(str(destination)):
+                    sftp.mkdir(str(destination))
+                
+                # TRANSFORM PATH OBJECT TO STRING, SINCE PYSFTP DOESNT SUPPORT IT    
+                source = str(source / filename)
+                destination = str(destination / filename)
+                sftp.put(source, destination)
+                os.remove(source)
+                    
+                return True
+        
+        except Exception as excep:
+            return False        
+    else:
+        pass
